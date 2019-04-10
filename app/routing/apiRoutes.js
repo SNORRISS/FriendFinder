3818f2c31a6fa8ac408135ecc99afcd22cbe3fd8
SNORISS <samuel.norris@marquette.edu>
2019-04-10 14:24:46 -0500

    home page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,29 @@
-app.get("/api/friends", function(req, res) {});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "friends"
+  });
+}
 
-app.get("/api/friends", function(req, res) {});
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    //once successfully connected, you may want to query your database for the info you'll need later!
+  }
+});
+
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    connection.query("SELECT * FROM profiles", function(data) {
+      res.json(data);
+    });
+  });
+
+  app.post("/api/friends", function(req, res) {});
+};
