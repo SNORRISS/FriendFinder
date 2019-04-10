93ae2d604e45aad33339b9b226ac1c2a3cb7ddfc
SNORISS <samuel.norris@marquette.edu>
2019-04-10 14:31:09 -0500

    fixed

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(express.static("app/public"));
 
 // Routes
 // =============================================================
-require("./app/routes/api-routes.js")(app);
-require("./app/routes/html-routes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
